7aae0c35e7ffbdf4e12c2042de40def8590336cd
MarcoAntonioNobre <0001000610@senaimgaluno.com.br>
2024-07-24 09:57:34 -0300

    Alteracoes nos nomes e nos arquivos de rota

--- admin/rota.php
+++ admin/rota.php
@@ -3,6 +3,17 @@ include_once("../config/constantes.php");
 include_once("../config/conexao.php");
 include_once("../func/funcoes.php");
 
-//$url = $_GET['url'] ?? 'dashboard';
-//$url = array_filter(explode('/', $url));
+$url = $_GET['url'] ?? 'erro';
+$url = array_filter(explode('/', $url));
 
+switch ($url[0]) {
+    case 'inicio':
+        include_once("dashboard.php");
+        break;
+    case 'visualizar-emprestimo':
+        include_once("verificarAluguel.php");
+        break;
+    default:
+        include_once('404.php');
+        break;
+}
