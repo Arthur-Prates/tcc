3b0fa4b3f64f3fe5af15abf3238f9b084942561d
MarcoAntonioNobre <0001000610@senaimgaluno.com.br>
2024-07-30 10:06:16 -0300

    Correcoes e tela de bem-vindo

--- admin/login.php
+++ admin/login.php
@@ -10,7 +10,6 @@ $email = $dados['email'];
 $senha = $dados['senha'];
 $retornoValidar = verificarSenhaCriptografada('*', 'usuario', 'email', $email, 'senha', $senha, 'ativo', 'A');
 
-
 if ($retornoValidar) {
     if ($retornoValidar === 'usuario') {
         echo json_encode(['success' => false, 'message' => 'Usuário inválido']);
@@ -22,7 +21,8 @@ if ($retornoValidar) {
             $_SESSION['idadm'] = $retornoValidar->idusuario;
             $_SESSION['nome'] = $retornoValidar->nomeUsuario;
             $_SESSION['emailAdm'] = $retornoValidar->email;
-            echo json_encode(['success' => true, 'message' => "Logado com sucesso!"]);
+            $_SESSION['cargo'] = $cargo;
+            echo json_encode(['success' => true, 'message' => "Logado com sucesso!",'nome'=>$_SESSION['nome']]);
         } else {
             unset($_SESSION['idadm']);
             echo json_encode(['success' => false, 'message' => "Você não pode acessar!"]);
